4b6868c9edb8cbd4e0644ef93218143887aef7f9
Rahul Dhangar <rahul.dhangar@meu.edu.in>
2024-02-13 09:44:39 +0530

    Add finished app for component basics

---
+++ 2-Components/classwork/2-finish/react-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
